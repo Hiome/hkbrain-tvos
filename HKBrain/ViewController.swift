4a1269d2f820b70a1904a29eb4fd3f3451c3c3e7
Neil Gupta <ngupta@instructure.com>
2018-02-24 21:02:12 -0600

    Initial Commit

---
+++ HKBrain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HKBrain
+//
+//  Created by Neil Gupta on 2/24/18.
+//  Copyright © 2018 Hiome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
